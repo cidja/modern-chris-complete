962952cb1bb4f471cde64bcc0a41260603650f83
cidja <chgeorgesc@gmail.com>
2022-08-04 17:49:35 +0200

    passage à 4 tuiles et ajout des sites aventure-rh ajjardin huclink

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # linternaute-averti
  accueil portfolio
  bla bla bla 
+
+taille des images pour home.php
+640*426
--- view/frontend/home.php
+++ view/frontend/home.php
@@ -12,122 +12,132 @@ ob_start();
         <h3 class="titleH3">bonne visite</h3>
       </div>
     </div>
-
     <div class='container-fluid' id="conteneur_portfolio">
 
+    <div class="row"> <!-- Début première ligne -->
+
+      <div class="col-md-3" id="elementgauche">
+        <div id="container" class="allContainerHome">
+          <a href="https://aventure-rh.com">
+            <figure class="c4-izmir c4-border-left" id="cadre_aventure-rh">
+              <img class="imgContainer rounded img-fluid" src="./public/img/aventure-rh_accueil.png" alt="page d'acceuil du site internet Aventure-rh.com">
+              <h3 class="textInnerImg" id="text">Site internet d'une entreprise de Ressources Humaines</h3>
+            </figure>
+          </a>
+        </div>
+      </div>
+
+      <div class="col-md-3" id="apparition">
+        <div id="container" class="allContainerHome">
+          <a href="https://ajjardin.fr">
+            <figure class="c4-izmir c4-border-center" id="cadre_ajjardin">
+              <img class="imgContainer rounded img-fluid" src="public/img/ajjardin_accueuil.png" alt="page d'acceuil du site internet Ajjardin.fr">
+              <h3 class="textInnerImg" id="text">Site internet d'une entreprise de jardinage</h3>
+            </figure>
+          </a>
+        </div>
+      </div>
+
+      <div class="col-md-3" id="apparition">
+        <div id="container" class="allContainerHome">
+          <a href="https://huclink.fr">
+            <figure class="c4-izmir c4-border-right" id="cadredroit">
+              <img class="imgContainer rounded img-fluid" src="./public/img/huclink.png" alt="page d'acceuil du site internet huclink.fr">
+              <h3 class="textInnerImg" id="text">Site internet de la startup hucLink la borne d'emploi</h3>
+            </figure>
+          </a>
+        </div>
+      </div>
+
+      <div class="col-md-3" id="elementdroite"> <!-- gauche -->
+        <div id="containerajHome" class="allContainerHome">
+            <a href="https://stone-4-you.fr">
+              <figure class="c4-izmir c4-border-center" id="cadreaj">
+                <img class="imgContainer rounded img-fluid" src="public/img/ajjardin_accueuil.png" alt="Coucou">
+                <h3 class="textInnerImg" id="textP5"> Site de e-commerce de vente de bijoux </h3>
+                <figcaption>
+                <div class="c4-reveal-left"> 
+                </div>
+                </figcaption>
+              </figure>
+            </a>
+          </div>
+        </div>
+
+    </div> <!-- Fin première ligne -->
+
+
+  <div class="row"> <!-- Début seconde ligne -->
+
+    <div class="col-md-3" id="elementgauche"> <!-- 5 -->
+    <div id="containerajHome" class="allContainerHome">
+    <a href="#">
+          <figure class="c4-izmir c4-border-center" id="cadrep5">
+            <img class="imgContainer rounded img-fluid" src="public/img/projet_commence.jpg" alt="Image d'un livre ouvert pour signifier que le projet est un projet de gestion de bibliothèque">
+            <h3 class="textInnerImg" id="textP5"> Votre projet commence ici </h3>
+            <figcaption>
+            
+            </figcaption>
+          </figure>
+        </a>
+      </div>
+    </div>
+
+    <div class="col-md-3" id="apparition"> <!-- 6 -->
+    <div id="containerp4Home" class="allContainerHome">
+    <a href="index.php?action=p5">
+          <figure class="c4-izmir c4-border-center" id="cadrep5">
+            <img class="imgContainer rounded img-fluid" src="./public/img/p5.jpg" alt="Image d'un livre ouvert pour signifier que le projet est un projet de gestion de bibliothèque">
+            <h3 class="textInnerImg" id="textP5"> Projet final gestion de bibliothèque </h3>
+            <figcaption>
+            
+            </figcaption>
+          </figure>
+        </a>
+      </div>
+    </div>
+
+    <div class="col-md-3" id="apparition"> <!-- 7 -->
+    <div id="containerp3Home" class="allContainerHome">
+        <a href='index.php?action=p3'>
+          <figure class="c4-izmir c4-border-bottom-left" id="cadrep3">
+            <img class='imgContainer rounded img-fluid' src='./public/img/p3.jpg' alt="projet 3 l'image est une femme blonde en débardeur et leggings de style californienne qui pose sur une route avec un vélo en premier plan et en arrière plan la mer" />
+            <h3 class="textInnerImg" id="textP3"><i class="fab fa-js-square"></i> Application de réservation de vélos</h3>
+            <figcaption>
+              <div class="c4-reveal-right">
+              </div>
+            </figcaption>
+          </figure>
+        </a>
+      </div> 
+    </div>
+
+
+    <div class="col-md-3" id="elementdroite"> <!-- 8 -->
+    <div id="containerp1Home" class="allContainerHome">
+        <a href='index.php?action=p1'>
+          <figure class="c4-izmir c4-border-top-left" id="cadrep1">
+            <img  class='imgContainer rounded img-fluid' src='./public/img/p1.jpg' alt='projet web agency, image montrant deux écran de pc allumé avec des lignes de code et un iphone est posé à la verticale sur la droite du bureau' />
+            <h3 class="textInnerImg" id="textP1"><i class="far fa-file-code"></i> Intégration d'une maquette graphique</h3>
+            <figcaption>
+            <div class="c4-reveal-right">
+            </div>
+          </figcaption>
+          </figure>
+        </a>
+      </div> 
+    </div>
     
 
+  </div> <!-- Fin de la seconde ligne -->
 
-
-    <div class="row">
-      <div class="col-md-4 offset-md-4" id="apparition">
-      <div id="containerp5Home" class="allContainerHome">
-            <a href="index.php?action=p5">
-              <figure class="c4-izmir c4-border-center" id="cadrep5">
-                <img class="imgContainer rounded img-fluid" src="./public/img/p5.jpg" alt="Image d'un livre ouvert pour signifier que le projet est un projet de gestion de bibliothèque">
-                <h3 class="textInnerImg" id="textP5"> Projet final gestion de bibliothèque </h3>
-                <figcaption>
-                
-                </figcaption>
-              </figure>
-            </a>
-          </div>
-      </div>
-    </div>
-
-
-      <div class="row">
-        <div class="col-md-4" id="elementhuclink">
-        <div id="containerajHome" class="allContainerHome">
-            <a href="https://huclink.fr">
-              <figure class="c4-izmir c4-border-center" id="cadrehuclink">
-                <img class="imgContainer rounded img-fluid" src="./public/img/huclink.png" alt="présentation du site hucLInk">
-                <h3 class="textInnerImg" id="textP5"> Site professionnel de la start-up hucLink </h3>
-                <figcaption>
-                
-                </figcaption>
-              </figure>
-            </a>
-          </div>
-        </div>
-        <div class="col-md-4" id="apparition">
-        <div id="containerp4Home" class="allContainerHome">
-            <a href="index.php?action=p4">
-              <figure class="c4-izmir c4-border-bottom-right" id="cadrep4">
-              <img class="imgContainer rounded img-fluid" src="./public/img/p4.jpg" alt="image montrant une main qui écrit sur un calepin avec un ordinateur portable en arrière plan">
-              <h3 class="textInnerImg" id="textP4"><i class="fab fa-php"></i> Blog d'écrivain PHP</h3>
-              <figcaption>
-                  <div class="c4-reveal-left"> 
-                  </div>
-                </figcaption>
-              </figure>
-            </a>
-          </div>
-        </div>
-        <div class="col-md-4" id="elementajjardin">
-        <div id="containerajHome" class="allContainerHome">
-            <a href="https://ajjardin.fr">
-              <figure class="c4-izmir c4-border-center" id="cadreaj">
-                <img class="imgContainer rounded img-fluid" src="./public/img/ajjardin.png" alt="">
-                <h3 class="textInnerImg" id="textP5"> Site Web d'une entreprise de jardinage </h3>
-                <figcaption>
-                
-                </figcaption>
-              </figure>
-            </a>
-          </div>
-        </div>
-      </div>
-
-
-      <div class="row">
-        <div class="col-md-4" id="elementp3">
-        <div id="containerp3Home" class="allContainerHome">
-            <a href='index.php?action=p3'>
-              <figure class="c4-izmir c4-border-bottom-left" id="cadrep3">
-                <img class='imgContainer rounded img-fluid' src='./public/img/p3.jpg' alt="projet 3 l'image est une femme blonde en débardeur et leggings de style californienne qui pose sur une route avec un vélo en premier plan et en arrière plan la mer" />
-                <h3 class="textInnerImg" id="textP3"><i class="fab fa-js-square"></i> Application de réservation de vélos</h3>
-                <figcaption>
-                  <div class="c4-reveal-right">
-                  </div>
-                </figcaption>
-              </figure>
-            </a>
-          </div> 
-        </div>
-        <div class="col-md-4" id="apparition">
-        <div id="containerp1Home" class="allContainerHome">
-            <a href='index.php?action=p1'>
-              <figure class="c4-izmir c4-border-top-left" id="cadrep1">
-                <img  class='imgContainer rounded img-fluid' src='./public/img/p1.jpg' alt='projet web agency, image montrant deux écran de pc allumé avec des lignes de code et un iphone est posé à la verticale sur la droite du bureau' />
-                <h3 class="textInnerImg" id="textP1"><i class="far fa-file-code"></i> Intégration d'une maquette graphique</h3>
-                <figcaption>
-                <div class="c4-reveal-right">
-                </div>
-              </figcaption>
-              </figure>
-            </a>
-          </div> 
-        </div>
-        <div class="col-md-4" id="elementp2">
-        <div id="containerp2Home" class="allContainerHome">
-            <a href='index.php?action=p2'>
-              <figure class="c4-izmir c4-border-top-right" id="cadrep2">
-                <img class='imgContainer rounded img' src='./public/img/p2.jpg' alt='projet wordpress image affiche le logo wordpress (la lettre w blanche dans un rond bleu) qui est entouré par une main droite ' />
-                <h3 class="textInnerImg" id="textP2"><i class="fab fa-wordpress-simple"></i> Modification d'un thème Wordpress</h3>
-                <figcaption>
-                  <div class="c4-reveal-left">
-                  </div>
-                </figcaption>
-              </figure> 
-            </a>
-          </div> 
-        </div>
-      </div>
-    </div>
+</div> <!-- fin du container -->
+    
+  
     <div class="text-center">Site conforme aux normes W3C test validé sur le site <a href="https://validator.w3.org">https://validator.w3.org</a></div>
   </div>
  
+
   <?php
 
   $content = ob_get_clean();
